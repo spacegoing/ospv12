50aa59b31e626ee46d6bf4feb97c178035a53079
spacegoing <spacegoing@gmail.com>
2024-08-20 10:23:14 +0800

    [finish] 720p inference sp ddp single without tile works

--- sample_t2v.sh
+++ sample_t2v.sh
@@ -1,12 +1,16 @@
 CUDA_VISIBLE_DEVICES=0 python opensora/sample/sample_t2v.py \
-    --save_img_path "./eval/single_500" \
-    --num_frames 93 \
+    --save_img_path "./eval/single_720_euler" \
     --height 720 \
     --width 1280 \
     --model_path "/workspace/public/models/Open-Sora-Plan-v1.2.0/93x720p/" \
+    # --save_img_path "./eval/single_480_ddpm" \
+    # --height 480 \
+    # --width 640 \
+    # --model_path "/workspace/public/models/Open-Sora-Plan-v1.2.0/93x480p/checkpoint-12000/model/" \
+    --text_prompt examples/test_mt5.txt \
+    --num_frames 93 \
     --cache_dir "../cache_dir" \
     --text_encoder_name "/workspace/host_folder/Open-Sora-Plan/google-mt5-xxl" \
-    --text_prompt examples/test_mt5.txt \
     --ae CausalVAEModel_D4_4x8x8 \
     --ae_path "/workspace/public/models/Open-Sora-Plan-v1.2.0/vae" \
     --fps 24 \
@@ -14,5 +18,7 @@ CUDA_VISIBLE_DEVICES=0 python opensora/sample/sample_t2v.py \
     --num_sampling_steps 100 \
     --enable_tiling \
     --max_sequence_length 512 \
+    # --sample_method DPMSolverMultistep \
+    # --sample_method DDPM \
     --sample_method EulerAncestralDiscrete \
     --model_type "dit"
--- sample_t2v_ddp.sh
+++ sample_t2v_ddp.sh
@@ -1,13 +1,14 @@
+
 torchrun --nnodes=1 --nproc_per_node 8  --master_port 29503 \
-    -m opensora.sample.sample_t2v_sp \
-    --save_img_path "./eval/sp_720_ddpm" \
+    -m opensora.sample.sample_t2v_ddp \
+    --save_img_path "./eval/ddp_720_euler" \
+    --text_prompt examples/test_mt5.txt \
     --num_frames 93 \
     --height 720 \
     --width 1280 \
     --model_path "/workspace/public/models/Open-Sora-Plan-v1.2.0/93x720p/" \
     --cache_dir "../cache_dir" \
     --text_encoder_name "/workspace/host_folder/Open-Sora-Plan/google-mt5-xxl" \
-    --text_prompt examples/test_mt5.txt \
     --ae CausalVAEModel_D4_4x8x8 \
     --ae_path "/workspace/public/models/Open-Sora-Plan-v1.2.0/vae" \
     --fps 24 \
@@ -16,7 +17,5 @@ torchrun --nnodes=1 --nproc_per_node 8  --master_port 29503 \
     --enable_tiling \
     # --tile_overlap_factor 0.125 \
     --max_sequence_length 512 \
-    # --sample_method DPMSolverMultistep \
-    --sample_method DDPM \
-    # --sample_method EulerAncestralDiscrete \
-    --model_type "dit" 
+    --sample_method EulerAncestralDiscrete \
+    --model_type "dit"
