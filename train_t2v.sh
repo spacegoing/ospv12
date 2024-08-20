f2e422120cc9b08274b579218aa414c5ce2ac2b1
spacegoing <spacegoing@gmail.com>
2024-08-20 15:44:48 +0800

    [New] pretrain model with warmup

--- train_t2v.sh
+++ train_t2v.sh
@@ -1,8 +1,10 @@
 # export WANDB_MODE="offline"
 export ENTITY="linbin"
-export HF_DATASETS_OFFLINE=1 
+export HF_DATASETS_OFFLINE=1
 export TRANSFORMERS_OFFLINE=1
 export PDSH_RCMD_TYPE=ssh
+export XDG_CACHE_HOME="/workspace/public/users/lichang93/.cache"
+export HF_HOME="/workspace/public/users/lichang93/.cache/huggingface"
 # NCCL setting
 # export GLOO_SOCKET_IFNAME=bond0
 export NCCL_SOCKET_IFNAME=eth0
@@ -20,13 +22,14 @@ export MKL_NUM_THREADS=1
 accelerate launch \
     --config_file scripts/accelerate_configs/deepspeed_zero2_config.yaml \
     opensora/train/train_t2v_diffusers.py \
-    --model OpenSoraT2V-ROPE-L/122 \
-    --text_encoder_name "/workspace/public/users/lichang93/mydocker/cogvx/host_folder/Open-Sora-Plan/google-mt5-xxl" \
-    --cache_dir "./cache_dir" \
-    --dataset t2v \
+    --sp_size 8 \
+    --train_sp_batch_size 2 \
+    --output_dir="/workspace/Open-Sora-Plan/runs/480p_warm100_1e4/" \
     --data "m5.txt" \
-    --ae CausalVAEModel_D4_4x8x8 \
-    --ae_path "/workspace/public/models/Open-Sora-Plan-v1.2.0/vae" \
+    --dataset t2v \
+    --cache_dir "./cache_dir" \
+    --pretrained "/workspace/public/models/Open-Sora-Plan-v1.2.0/93x480p/diffusion_pytorch_model.safetensors" \
+    --model OpenSoraT2V-ROPE-L/122 \
     --sample_rate 1 \
     --num_frames 93 \
     --max_height 480 \
@@ -42,7 +45,7 @@ accelerate launch \
     --max_train_steps=1000000 \
     --learning_rate=1e-4 \
     --lr_scheduler="constant" \
-    --lr_warmup_steps=0 \
+    --lr_warmup_steps=100 \
     --mixed_precision="bf16" \
     --report_to="wandb" \
     --checkpointing_steps=250 \
@@ -56,11 +59,11 @@ accelerate launch \
     --cfg 0.1 \
     --noise_offset 0.02 \
     --use_rope \
-    --resume_from_checkpoint="latest" \
     --ema_decay 0.999 \
     --enable_tiling \
     --speed_factor 1.0 \
     --group_frame \
-    --sp_size 8 \
-    --train_sp_batch_size 2 \
-    --output_dir="/workspace/public/models/Open-Sora-Plan-v1.2.0/test"
+    --text_encoder_name "/workspace/public/users/lichang93/mydocker/cogvx/host_folder/Open-Sora-Plan/google-mt5-xxl" \
+    --ae CausalVAEModel_D4_4x8x8 \
+    --ae_path "/workspace/public/models/Open-Sora-Plan-v1.2.0/vae"
+    # --resume_from_checkpoint="latest" \
