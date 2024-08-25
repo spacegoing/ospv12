0f52563f85e7c10dfa890601e56546143f3e82c0
spacegoing <spacegoing@gmail.com>
2024-08-25 13:36:49 +0000

    [New] test 1node 8gpus args

--- mytest/run_1g_test.sh
+++ mytest/run_1g_test.sh
@@ -7,16 +7,16 @@ export XDG_CACHE_HOME="/workspace/public/users/lichang93/.cache"
 export HF_HOME="/workspace/public/users/lichang93/.cache/huggingface"
 # NCCL setting
 # export GLOO_SOCKET_IFNAME=bond0
-export NCCL_SOCKET_IFNAME=eth0
-export NCCL_IB_HCA=mlx5_10:1,mlx5_11:1,mlx5_12:1,mlx5_13:1
-export NCCL_IB_GID_INDEX=3
-export NCCL_IB_TC=162
-export NCCL_IB_TIMEOUT=22
-export NCCL_PXN_DISABLE=0
-export NCCL_IB_QPS_PER_CONNECTION=4
-export NCCL_ALGO=Ring
-export OMP_NUM_THREADS=1
-export MKL_NUM_THREADS=1
+# export NCCL_SOCKET_IFNAME=eth0
+# export NCCL_IB_HCA=mlx5_10:1,mlx5_11:1,mlx5_12:1,mlx5_13:1
+# export NCCL_IB_GID_INDEX=3
+# export NCCL_IB_TC=162
+# export NCCL_IB_TIMEOUT=22
+# export NCCL_PXN_DISABLE=0
+# export NCCL_IB_QPS_PER_CONNECTION=4
+# export NCCL_ALGO=Ring
+# export OMP_NUM_THREADS=1
+# export MKL_NUM_THREADS=1
 # export NCCL_ALGO=Tree
 
 accelerate launch \
