9a79acfa1e6c3e9a34965c8bbb3d1849ed67ddf3
spacegoing <spacegoing@gmail.com>
2024-08-26 14:11:26 +0000

    [New] batch inf shell use watch mode

--- myscripts/batch_inf.sh
+++ myscripts/batch_inf.sh
@@ -1,23 +1,24 @@
 #!/bin/bash
 
 # Main variables
-model_name="sub567_480"
+model_name="bricks_long_bsz16"
 CHECKPOINT_DIR="/workspace/public/users/lichang93/mydocker/cogvx/host_folder/Open-Sora-Plan/runs/$model_name"
 declare -a specific_ckpts=()  # List of specific checkpoint numbers to evaluate
+declare -a processed_ckpts=() # List to keep track of processed checkpoints
 
 # Function to run evaluation using torchrun
 run_evaluation() {
     local model_name=$1
     local ckpt_num=$2
     local model_path=$3
-    local save_img_path="./eval/${model_name}_ckpt_${ckpt_num}"
+    local save_img_path="./eval/${model_name}/${model_name}_ckpt_${ckpt_num}"
 
     # Execute the torchrun command with provided arguments
     torchrun --nnodes=1 --nproc_per_node 8  --master_port 29503 \
              -m opensora.sample.sample_t2v_sp \
              --save_img_path "$save_img_path" \
              --model_path "$model_path" \
-             --text_prompt 'myprompts/fridge_c6_5.txt' \
+             --text_prompt 'myprompts/bricks.txt' \
              --num_frames 93 \
              --height 480 \
              --width 640 \
@@ -38,23 +39,19 @@ run_evaluation() {
 function check_and_run {
     local ckpt_num=$1
     local ckpt_folder="$CHECKPOINT_DIR/checkpoint-$ckpt_num"
-    if [ -d "$ckpt_folder" ]; then
+    if [ -d "$ckpt_folder" ] && [[ ! " ${processed_ckpts[@]} " =~ " $ckpt_num " ]]; then
         run_evaluation "$model_name" "$ckpt_num" "$ckpt_folder/model"
+        processed_ckpts+=("$ckpt_num")  # Mark this checkpoint as processed
     else
-        echo "Checkpoint directory $ckpt_folder does not exist."
+        echo "Checkpoint directory $ckpt_folder does not exist or already processed."
     fi
 }
 
-# Check if specific checkpoints are provided
-if [ ${#specific_ckpts[@]} -ne 0 ]; then
-    # Evaluate specific checkpoints only
-    for ckpt_num in "${specific_ckpts[@]}"; do
-        check_and_run "$ckpt_num"
-    done
-else
-    # Iterate over all checkpoint folders
+# Main loop to continuously monitor for new checkpoints
+while true; do
     for ckpt_folder in $CHECKPOINT_DIR/checkpoint-*; do
         ckpt_num=$(basename $ckpt_folder | sed 's/checkpoint-//')
         check_and_run "$ckpt_num"
     done
-fi
+    sleep 600  # Check every 10 minutes
+done
